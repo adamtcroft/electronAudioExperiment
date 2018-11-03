d3d57474bbaac602497d29b9739307df98248472
Adam T. Croft <sendtoatc@gmail.com>
2018-11-02 21:35:22 -0700

    basic crappy audio player complete!

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const electron = require('electron');
-const {app, BrowserWindow, ipcMain, dialog} = electron;
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
 
-let win;
-let openDialogOptions = {
+let mainWindow;
+let openFileDialogOptions = {
     title: "Choose an Audio File",
-    filters:[
-        {name: 'Audio Files', extensions: ['wav', 'mp3']}
+    filters: [
+        { name: 'Audio Files', extensions: ['wav', 'mp3'] }
     ]
 };
+let selectedFilePath;
 
-function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow()
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow()
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
 }
 
 app.on('ready', createWindow)
 
 ipcMain.on('Open', (event) => {
-    dialog.showOpenDialog(openDialogOptions, (filenames) =>{
-
+    dialog.showOpenDialog(openFileDialogOptions, function (filepath) {
+        selectedFilePath = filepath[0];
     });
+});
+
+ipcMain.on('Play', (event) => {
+    mainWindow.webContents.send('PlaySound', selectedFilePath);
+});
+
+ipcMain.on('Stop', (event) => {
+    mainWindow.webContents.send('StopSound');
 });
