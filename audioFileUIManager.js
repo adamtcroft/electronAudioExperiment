54fe5d0477fcc2cf71f937601f2bb2ab893c2f83
Adam T. Croft <sendtoatc@gmail.com>
2018-11-10 23:02:12 -0800

    even more changes and UI is jacked up for drawing waveforms

--- audioFileUIManager.js
+++ audioFileUIManager.js
@@ -1,7 +1,7 @@
 class AudioFileUIManager {
     UpdateFileListUI(audioFileList) {
         let fileListCollection = document.getElementById("fileList");
-        
+
         while(fileListCollection.firstChild){
             fileListCollection.removeChild(fileListCollection.firstChild);
         }
@@ -9,13 +9,23 @@ class AudioFileUIManager {
         audioFileList.forEach((file) => {
             let anchor = document.createElement("a");
             anchor.className = "collection-item";
-            anchor.onclick = () => { anchor.className = "collection-item active" };
+            anchor.onclick = () => { 
+                this.IsSomethingSelectedAlready();
+                anchor.classList.add("active");
+            };
             let n = file.lastIndexOf('\\');
             let filenameSubstring = file.substring(n + 1);
             let value = document.createTextNode(filenameSubstring);
             anchor.appendChild(value);
             fileListCollection.appendChild(anchor);
         });
+    }
+
+    IsSomethingSelectedAlready(){
+        let anchor = document.getElementsByClassName("active");
+        if(anchor.length != 0){
+            anchor[0].classList.remove("active");
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function Stop() {
     wavefile.stop();
 }
 
-function DrawWave() {
+function DrawWave(file) {
     wavefile = WaveSurfer.create({
         container: '#waveform',
         waveColor: 'white',
@@ -26,7 +26,37 @@ function DrawWave() {
         responsive: true,
         height: 40
     });
-    wavefile.load(filepath);
+    wavefile.load(file);
+}
+
+function UpdateFileListUI(audioFileList) {
+    let fileListCollection = document.getElementById("fileList");
+
+    while (fileListCollection.firstChild) {
+        fileListCollection.removeChild(fileListCollection.firstChild);
+    }
+
+    audioFileList.forEach((file) => {
+        let anchor = document.createElement("a");
+        anchor.className = "collection-item";
+        anchor.onclick = () => {
+            IsSomethingSelectedAlready();
+            anchor.classList.add("active");
+            DrawWave(file);
+        };
+        let n = file.lastIndexOf('\\');
+        let filenameSubstring = file.substring(n + 1);
+        let value = document.createTextNode(filenameSubstring);
+        anchor.appendChild(value);
+        fileListCollection.appendChild(anchor);
+    });
+}
+
+function IsSomethingSelectedAlready() {
+    let anchor = document.getElementsByClassName("active");
+    if (anchor.length != 0) {
+        anchor[0].classList.remove("active");
+    }
 }
 
 module.exports.Open = Open;
@@ -34,3 +64,4 @@ module.exports.Play = Play;
 module.exports.Pause = Pause;
 module.exports.Stop = Stop;
 module.exports.DrawWave = DrawWave;
+module.exports.UpdateFileListUI = UpdateFileListUI;
